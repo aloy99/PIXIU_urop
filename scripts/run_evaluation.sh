ea755f8906aa114d1d82fffd3d5198a76e1f6b1f
aloy99 <aloysiuschowhc@gmail.com>
2024-03-30 02:53:49 +0000

    Update eval script

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -3,7 +3,7 @@ export PYTHONPATH="$pixiu_path/src:$pixiu_path/src/financial-evaluation:$pixiu_p
 echo $PYTHONPATH
 export CUDA_VISIBLE_DEVICES="0"
 
-declare -a tasks=("flare_fpb")
+declare -a tasks=("flare_fpb" "flare_fiqasa" "flare_finqa" "flare_convfinqa" "flare_sm_bigdata" "flare_sm_acl" "flare_sm_cikm")
 
 now="$(date +'%Y-%m-%d-%T')"
 start=$(date +%s)
@@ -13,10 +13,22 @@ do
     python3 src/eval.py \
         --model hf-causal-vllm \
         --tasks $TASK \
-        --model_args use_accelerate=True,pretrained=./fingpt,tokenizer=./fingpt,use_fast=False \
+        --model_args use_accelerate=True,pretrained=AdaptLLM/finance-chat,tokenizer=AdaptLLM/finance-chat,use_fast=False \
         --no_cache \
         --batch_size 8 \
         --num_fewshot 0  >> output_"$now".log
+done
+
+declare -a fewshot_tasks=("flare_ner" "flare_headlines")
+for TASK in "${tasks[@]}"
+do
+    python3 src/eval.py \
+        --model hf-causal-vllm \
+        --tasks $TASK \
+        --model_args use_accelerate=True,pretrained=AdaptLLM/finance-chat,tokenizer=AdaptLLM/finance-chat,use_fast=False \
+        --no_cache \
+        --batch_size 8 \
+        --num_fewshot 5  >> output_"$now".log
 done
 end=$(date +%s)
 seconds=$(echo "$end - $start" | bc)
